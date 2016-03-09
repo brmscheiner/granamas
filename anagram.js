2b587b7a05d6b1fe1ea7e8f1f25a42becca82b13
ben <brmscheiner@gmail.com>
2016-03-09 18:57:51 -0500

    animation skeleton

--- anagram.js
+++ anagram.js
@@ -1,64 +1,89 @@
 "use strict"
 
-class Anagram = {
+class Anagram {
   constructor() {
     this.config = {}
+    this.counter = 0
     this.charData = []
     this.lastState = null
   }
 
-  initialize(configDictionary) {
-    this.config = configDictionary
+  animateTo(stateKey) {
+    console.log(stateKey)
+    config.nSteps = config.duration * 60 // 60 fps
+    for (i=0; i<this.allChars.length; i++) { // this is pseudocode
+      i.xStep = (i["state2"].x - i["state1"].x)/config.nSteps + i["state1"].x
+      i.yStep = (i["state2"].y - i["state1"].y)/config.nSteps + i["state1"].y
+    }
+    // window.requestAnimationFrame()
+  }
 
-    for (let text in this.config.states) {
-      if (!this.lastState) {
-        this.renderState(text, "visible")
-        this.lastState = state
-      } else {
-        this.renderState(text, "hidden")
-      }
+  step(timestamp) {
+    for (i=0; i<allChars.length; i++) {
+      this.animationControl(i)
+    }
+    if (this.counter < this.config.nSteps) {
+      requestAnimationFrame(this.step)
+      this.counter++
+    } else {
+      this.counter = 0
     }
   }
 
-  renderState(state, display) {
+  animationControl(charObj) {
+    element.style.transform = "translate(" + i.xStep +"px, " + i.yStep + "px)"
+  }
+
+  getCharData(charSpan) {
+    let charData = {}
+    charData.char = charSpan.innerHTML
+    charData.x = charSpan.offsetLeft
+    charData.y = charSpan.offsetTop
+    return charData
+  }
+
+  initialize(configDictionary) {
+    this.config = configDictionary
+    let stateKeys = Object.keys(this.config.states)
+
+    stateKeys.forEach((stateKey, i) => {
+      if (!this.lastState) {
+        this.renderState(stateKey, "visible")
+        this.lastState = stateKey
+      } else {
+        this.renderState(stateKey, "hidden")
+      }
+    })
+  }
+
+  renderState(stateKey, display) {
+    let text = this.config.states[stateKey]
     let charSpans = this.separateChars(text)
-    let charData = initialCharSpans.map(getCharData)
+    this.charData[stateKey] = charSpans.map((charSpan) => this.getCharData(charSpan))
   }
 
   separateChars(text) {
     let wordArray = text.split(' ')
-    wordArray.forEach(())
-    let charSpans = charArray.map((char, i) => {
-      let charSpan = document.createElement("span")
-          charSpan.className = "char"
-          charSpan.innerHTML = char
-      this.config.container.appendChild(charSpan)
-      return charSpan
+    let charSpans = []
+    wordArray.forEach((word, i) => {
+      let wordSpan = document.createElement("span")
+          wordSpan.className = "word"
+      let charArray = word.split('')
+          charArray.push(' ')
+      charArray.forEach((char, i) => {
+        let charSpan = document.createElement("span")
+            charSpan.className = "char"
+            charSpan.innerHTML = char
+        wordSpan.appendChild(charSpan)
+        charSpans.push(charSpan)
+      })
+      this.config.container.appendChild(wordSpan)
     })
     return charSpans
   }
 
-  // Re-render the config.container as separate spans
 
-  step(timestamp) {
-    if (!start) start = timestamp;
-    var progress = timestamp - start;
-    let pct = progress / config.duration*1000;
-    for (i=0; i<allChars.length; i++) {
-      this.animationControl(i,pct)
-    }
-    if (pct<100) {
-      requestAnimationFrame(this.step)
-    }
-  }
 
-  animationControl(charObj,pct) {
-    element.style.transform = "translate()"
-  }
 
-  Anagram.animateTo("stateName") => {}
-    // then, calculate the positions of all chars
-    this.create
-    window.requestAnimationFrame(this.step)
-  }
+
 }
