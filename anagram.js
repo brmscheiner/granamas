eb9821168dafb41a67737f64048af609f30201d1
ben <brmscheiner@gmail.com>
2016-03-09 18:10:06 -0500

    adding requestanimationframe logic

--- anagram.js
+++ anagram.js
@@ -1,67 +1,64 @@
 "use strict"
 
-class Anagram {
+class Anagram = {
   constructor() {
     this.config = {}
     this.charData = []
     this.lastState = null
   }
 
-  animationControl(timestampe,state1,state2) {
-    if (!start) start = timestamp;
-    let progress = timestamp - start;
-  }
-
-  animateTo(state) {
-    // then, calculate the positions of all chars
-    window.requestAnimationFrame()
-  }
-
-  getCharData(charSpan) {
-    let charData = {}
-    charData.char = charSpan.innerHTML
-    charData.x = charSpan.offsetLeft
-    charData.y = charSpan.offsetTop
-    return charData
-  }
-
   initialize(configDictionary) {
     this.config = configDictionary
-    let stateKeys = Object.keys(this.config.states)
 
-    for (let i in stateKeys) {
+    for (let text in this.config.states) {
       if (!this.lastState) {
-        this.renderState(i, "visible")
-        this.lastState = i
+        this.renderState(text, "visible")
+        this.lastState = state
       } else {
-        this.renderState(i, "hidden")
+        this.renderState(text, "hidden")
       }
     }
   }
 
   renderState(state, display) {
-    let text = this.config.states[state]
     let charSpans = this.separateChars(text)
-    this.charData[state] = charSpans.map((charSpan) => getCharData(charSpan))
+    let charData = initialCharSpans.map(getCharData)
   }
 
   separateChars(text) {
     let wordArray = text.split(' ')
-    let charSpans = []
-    wordArray.forEach((word, i) => {
-      let wordSpan = document.createElement("span")
-          wordSpan.className = "word"
-      let charArray = word.split('')
-          charArray.push(' ')
-      charArray.forEach((char, i) => {
-        let charSpan = document.createElement("span")
-            charSpan.className = "char"
-            charSpan.innerHTML = char
-        wordSpan.appendChild(charSpan)
-        charSpans.push(charSpan)
-      })
-      this.config.container.appendChild(wordSpan)
+    wordArray.forEach(())
+    let charSpans = charArray.map((char, i) => {
+      let charSpan = document.createElement("span")
+          charSpan.className = "char"
+          charSpan.innerHTML = char
+      this.config.container.appendChild(charSpan)
+      return charSpan
     })
     return charSpans
+  }
+
+  // Re-render the config.container as separate spans
+
+  step(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start;
+    let pct = progress / config.duration*1000;
+    for (i=0; i<allChars.length; i++) {
+      this.animationControl(i,pct)
+    }
+    if (pct<100) {
+      requestAnimationFrame(this.step)
+    }
+  }
+
+  animationControl(charObj,pct) {
+    element.style.transform = "translate()"
+  }
+
+  Anagram.animateTo("stateName") => {}
+    // then, calculate the positions of all chars
+    this.create
+    window.requestAnimationFrame(this.step)
   }
 }
